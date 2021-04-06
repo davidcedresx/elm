36162a5e8437325c1829d0807459c6b64b9167b3
David Cedres <david.cedresx@gmail.com>
2021-04-05 20:00:08 -0400

    add authenticated type

--- auth/src/Main.elm
+++ auth/src/Main.elm
@@ -3,7 +3,7 @@ module Main exposing (..)
 import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Html.Events exposing (onInput)
+import Html.Events exposing (onClick, onInput)
 
 
 main : Program () Model Msg
@@ -11,15 +11,18 @@ main =
     Browser.sandbox { init = init, update = update, view = view }
 
 
+type Auth
+    = Authenticated
+    | Guest { username : String, password : String }
+
+
 type alias Model =
-    { username : String
-    , password : String
-    }
+    Auth
 
 
 init : Model
 init =
-    Model "" ""
+    Guest { username = "", password = "" }
 
 
 
@@ -29,16 +32,23 @@ init =
 type Msg
     = Username String
     | Password String
+    | Logged
 
 
 update : Msg -> Model -> Model
 update msg model =
-    case msg of
-        Username username ->
-            { model | username = username }
+    case ( msg, model ) of
+        ( Username username, Guest guest ) ->
+            Guest { guest | username = username }
 
-        Password password ->
-            { model | password = password }
+        ( Password password, Guest guest ) ->
+            Guest { guest | password = password }
+
+        ( Logged, _ ) ->
+            Authenticated
+
+        _ ->
+            model
 
 
 
@@ -54,24 +64,35 @@ withStyle html =
         , html
         ]
 
+withLayout : Html msg -> Html msg
+withLayout html =
+    div [class "container"] 
+    [
+        html
+    ]
+    |> withStyle
+
 
 view : Model -> Html Msg
 view model =
-    div [ class "columns is-centered mt-6" ]
-        [ div [ class "column is-narrow" ]
-            [ h1 [ class "title is-1" ] [ text "Login" ]
-            , div [ class "field" ]
-                [ div [ class "control" ]
-                    [ input [ type_ "text", class "input", placeholder "username", value model.username, onInput Username ] []
+    case model of
+        Guest guest ->
+            div [ class "columns mt-6" ]
+                [ div [ class "column is-narrow" ]
+                    [ h1 [ class "title is-1" ] [ text "Login" ]
+                    , div [ class "field" ]
+                        [ div [ class "control" ]
+                            [ input [ type_ "text", class "input", placeholder "username", value guest.username, onInput Username ] []
+                            ]
+                        ]
+                    , div [ class "field" ]
+                        [ div [ class "control" ]
+                            [ input [ type_ "password", class "input", placeholder "password", value guest.password, onInput Password ] []
+                            ]
+                        ]
+                    , button [ type_ "submit", class "button is-primary", onClick Logged ] [ text "Submit" ]
                     ]
                 ]
-            , div [ class "field" ]
-                [ div [ class "control" ]
-                    [ input [ type_ "password", class "input", placeholder "password", value model.password, onInput Password ] []
-                    ]
-                ]
-            , button [ type_ "submit", class "button is-primary" ] [ text "Submit" ]
-            , p [] [ text model.username ]
-            ]
-        ]
-        |> withStyle
+                |> withLayout
+        Authenticated ->
+            h1 [ class "title is-1"] [ text "Welcome!"] |> withLayout
